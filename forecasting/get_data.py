ff19d4de916c7a6f915c21fa6506f2b026294943
Prashant Saxena <psaxena85@gmail.com>
2025-06-26 11:11:24 +0100

    Adding library files to start creating a package

---
+++ forecasting/get_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+
+
+def get_historical_fr_data(start_date: str, end_date: str) -> pd.DataFrame:
+    """
+    Collects historical frequency response data from the NESO API.
+    Returns a dataframe of price and volume.
+
+
+    
+    """
+    return 0  # Placeholder for actual implementation
